a26fb9e44451c352526a79993a4a3f3a29b1b09c
abizaregi <abizaregi21@gmail.com>
2022-04-17 14:27:40 +0700

    updated height

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -11,10 +11,10 @@ const Footer = () => {
         <img src={wave} alt="" style={{width: '100%'}}/>
         <div className="f-content">
             <div className="f-icons">
-                <Insta color='red' size='3rem'/>
-                <Whatsapp color='green' size='3rem'/>
-                <Github color='black' size='3rem'/>
-                <LinkedIn color='dodgerblue' size='3rem'/>
+                <a target='_blank' href="https://www.instagram.com/abizaregi"><Insta color='red' size='3rem'/></a>
+                <a target='_blank' href="https://api.whatsapp.com/send/?phone=6289515192171&text&app_absent=0"><Whatsapp color='green' size='3rem'/></a>
+                <a target='_blank' href="https://github.com/abizaregi"><Github color='black' size='3rem'/></a>
+                <a target='_blank' href="https://www.linkedin.com/in/abizar-egi-mahendra/"><LinkedIn color='dodgerblue' size='3rem'/></a>
             </div>
             <span>Created with &#9825; by 
                 <a href="mailto:abizaregi21@gmail.com"> 
